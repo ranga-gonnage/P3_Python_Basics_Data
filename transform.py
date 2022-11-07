b0f842d68986c500df21db55273c17537b029dad
Ranga Gonnage <rangagonnage@MacBook-Pro-de-Ranga.local>
2022-11-07 16:45:09 +0100

    Add requirements.txt and flake8 configuration file

--- transform.py
+++ transform.py
@@ -7,6 +7,7 @@ def transform(extracted_data):
         add_calculations(clean_data)
     return clean_data
 
+
 def remove_data(data, clean_data):
     clean_data["date"] = data["date_of_interest"]
     clean_data["case_count"] = data["case_count"]
@@ -18,37 +19,51 @@ def remove_data(data, clean_data):
     clean_data["hosp_count_7day_avg"] = data["hosp_count_7day_avg"]
     clean_data["death_count_7day_avg"] = data["all_death_count_7day_avg"]
 
+
 def convert_date(clean_data):
-    clean_data["date"] = clean_data["date"].split('T')[0]
+    clean_data["date"] = clean_data["date"].split("T")[0]
+
 
 def convert_to_int(clean_data):
     for key, value in clean_data.items():
-        if key is not "date":
+        if key != "date":
             clean_data[key] = int(value)
 
+
 def grouped_counted_cases(clean_data):
-    clean_data["case_count"] = clean_data["case_count"] + clean_data["probable_case_count"]
+    clean_data["case_count"] = (
+        clean_data["case_count"] + clean_data["probable_case_count"]
+    )
     del clean_data["probable_case_count"]
 
+
 def grouped_counted_death_cases(clean_data):
-    clean_data["death_count"] = clean_data["death_count"] + clean_data["probable_death_count"]
+    clean_data["death_count"] = (
+        clean_data["death_count"] + clean_data["probable_death_count"]
+    )
     del clean_data["probable_death_count"]
 
+
 def calculate_death_pourcent(clean_data):
-    pourcent =  clean_data["death_count"] / clean_data["case_count"] * 100
-    pourcent_7days_avg = clean_data["death_count_7day_avg"] / clean_data["case_count_7day_avg"] * 100
+    pourcent = clean_data["death_count"] / clean_data["case_count"] * 100
+    pourcent_7days_avg = (
+        clean_data["death_count_7day_avg"] / clean_data["case_count_7day_avg"] * 100
+    )
     clean_data["death_pourcent"] = round(pourcent, 2)
     clean_data["death_7days_avg_pourcent"] = round(pourcent_7days_avg, 2)
 
+
 def calculate_hospitalized_pourcent(clean_data):
-    pourcent =  clean_data["hospitalized_count"] / clean_data["case_count"] * 100
-    pourcent_7days_avg = clean_data["hosp_count_7day_avg"] / clean_data["case_count_7day_avg"] * 100
+    pourcent = clean_data["hospitalized_count"] / clean_data["case_count"] * 100
+    pourcent_7days_avg = (
+        clean_data["hosp_count_7day_avg"] / clean_data["case_count_7day_avg"] * 100
+    )
     clean_data["hospitalized_pourcent"] = round(pourcent, 2)
     clean_data["hospitalized_7days_avg_pourcent"] = round(pourcent_7days_avg, 2)
+
 
 def add_calculations(clean_data):
     grouped_counted_cases(clean_data)
     grouped_counted_death_cases(clean_data)
     calculate_death_pourcent(clean_data)
     calculate_hospitalized_pourcent(clean_data)
-
